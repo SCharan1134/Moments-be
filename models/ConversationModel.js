092b4f0a5caacb954a78ddce11358b9f6b1a2667
SCharan1134 <sricharanrayala24@gmail.com>
2024-04-26 22:04:19 +0530

    added chatseen

--- models/ConversationModel.js
+++ models/ConversationModel.js
@@ -2,19 +2,15 @@ import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema(
   {
-    participants: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    lastMessage: {
+      text: String,
+      sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      seen: {
+        type: Boolean,
+        default: false,
       },
-    ],
-    messages: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Message",
-        default: [],
-      },
-    ],
+    },
   },
   { timestamps: true }
 );
